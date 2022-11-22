f88f4d8b7a9e3dd8e4b59607ebcb8f7f408867ff
KarVarr <megatr9n@gmail.com>
2022-11-22 19:57:34 +0300

    Initial Commit

---
+++ H4X0RNews/H4X0RNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0RNewsApp.swift
+//  H4X0RNews
+//
+//  Created by Karen Vardanian on 22.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct H4X0RNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
